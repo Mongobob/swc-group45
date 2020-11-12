11eb39a6415e4aa0aa367c24bcd7c3850beba445
pbofataf7 <71585688+pbofataf7@users.noreply.github.com>
2020-11-12 13:28:02 +0100

    Added my first test. Up and running but might be subject to change. We'll see.

--- Assingment 04/tests/assignment04/WebTechnicianTest.java
+++ Assingment 04/tests/assignment04/WebTechnicianTest.java
@@ -1,14 +1,32 @@
 package assignment04;
 
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class WebTechnicianTest {
 
     @Test
-    void getIDNumber() {
-        System.out.println("Hello");
+    @DisplayName("Basic Functionality Check for Generator and Call Methods")
+    void FunctionalityCheck() {
+        String[] firstNames = {"James", "Sarah", "Jon"};
+        String[] lastNames = {"Smith", "Greenwood", "Oliver"};
+        int[] ids = {1, 2, 3};
+        List<WebTechnician> staff = new ArrayList<>();
+
+        for (int i = 0; i < firstNames.length; i++) {
+            staff.add(new WebTechnician(firstNames[i], lastNames[i], ids[i]));
+        }
+
+        for (int i = 0; i < staff.size(); i++) {
+            Assertions.assertEquals(firstNames[i], staff.get(i).getName());
+            Assertions.assertEquals(lastNames[i], staff.get(i).getSurname());
+            Assertions.assertEquals(ids[i], staff.get(i).getIDNumber());
+        }
+
     }
 
     @Test
