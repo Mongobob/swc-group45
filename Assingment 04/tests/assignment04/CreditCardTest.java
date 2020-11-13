f16825712ef12e6d0102794f88fbc79e8a583a13
pbofataf7 <71585688+pbofataf7@users.noreply.github.com>
2020-11-13 17:51:23 +0100

    Final version

--- Assingment 04/tests/assignment04/CreditCardTest.java
+++ Assingment 04/tests/assignment04/CreditCardTest.java
@@ -1,44 +1,59 @@
-package TestCases.assignment04;
+package assignment04;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.function.BooleanSupplier;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreditCardTest {
 
+    CreditCard c = new CreditCard("Hans","Muster", CustomerCounter.getInstance().getCustomerID(), "123", CreditCard.CreditCardType.regular);
+
     @Test
     void getName() {
+        assertEquals("Hans",c.getName());
     }
 
     @Test
     void getSurname() {
+        assertEquals("Muster",c.getSurname());
     }
 
     @Test
     void getSerialNumber() {
+        assertEquals(CustomerCounter.getInstance().getCustomerID(),c.getSerialNumber());
     }
 
     @Test
     void getExpirationDate() {
+        assertTrue((BooleanSupplier) c.getExpirationDate());
     }
 
     @Test
     void getSecurityCode() {
+        assertEquals("123",c.getSecurityCode());
+
     }
 
     @Test
     void getCCType() {
+        assertEquals(CreditCard.CreditCardType.regular,c.getCCType());
     }
 
     @Test
     void changeCCType() {
+        c.changeCCType(CreditCard.CreditCardType.gold);
+        assertEquals(CreditCard.CreditCardType.gold, c.getCCType());
     }
 
     @Test
     void getLimit() {
+        assertEquals(2000, c.getLimit());
     }
 
     @Test
     void isExpired() {
+        assertTrue(c.isExpired());
     }
 }
--- Assingment 04/tests/assignment04/CustomerTest.java
+++ Assingment 04/tests/assignment04/CustomerTest.java
@@ -1,64 +1,80 @@
 package assignment04;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CustomerTest {
+public class CustomerTest {
+
+    private Customer d;
+    private Customer e;
+    private Customer f;
+
+    // Set up - Called before each method to generate customer
+    @BeforeEach
+    public void setUp() {
+        // clearing all in case there was something left
+        Archive.getInstance().reset();
+        CustomerCounter.getInstance().resetID();
+        EmployeeCounter.getInstance().resetID();
+
+        // add one employee to handle all the customers
+        RegularEmployee a = new RegularEmployee("Jon", "Smith");
+
+        // initialize various customers
+        d = new Customer("Gloria", "Gloris", "01/01/2020", 1200000, Customer.CustomerType.platinum, "777");
+        e = new Customer("Stephen", "Colbert", "13/06/1976", 400000, Customer.CustomerType.regular, "467");
+        f = new Customer("Ru", "Paul", "12/05/1986", 2800000, Customer.CustomerType.gold, "867");
+    }
+
+    // Tear down - Called after every test method.
+    @AfterEach
+    public void tearDown() {
+        // clearing all in case there was something left
+        Archive.getInstance().reset();
+        CustomerCounter.getInstance().resetID();
+        EmployeeCounter.getInstance().resetID();
+    }
 
     @Test
     void getName() {
+        Assertions.assertEquals("Gloria", d.getName());
+        Assertions.assertEquals("Stephen", e.getName());
+        Assertions.assertEquals("Ru", f.getName());
     }
 
     @Test
     void getSurname() {
+        Assertions.assertEquals("Gloris", d.getSurname());
+        Assertions.assertEquals("Colbert", e.getSurname());
+        Assertions.assertEquals("Paul", f.getSurname());
     }
 
     @Test
-    void getSavings() {
+    void getExpirationDate() {
     }
 
     @Test
-    void getBankAccountNumber() {
+    void getSecurityCode() {
     }
 
     @Test
-    void getId() {
+    void getCCType() {
     }
 
     @Test
-    void getEmployeeID() {
+    void changeCCType() {
     }
 
     @Test
-    void getMyType() {
+    void getLimit() {
     }
 
     @Test
-    void getCC() {
+    void isExpired() {
     }
 
-    @Test
-    void getAge() {
-    }
-
-    @Test
-    void deposit() {
-    }
-
-    @Test
-    void withdraw() {
-    }
-
-    @Test
-    void payingWithTransfer() {
-    }
-
-    @Test
-    void payingWithCC() {
-    }
-
-    @Test
-    void changeCustomerType() {
-    }
 }
