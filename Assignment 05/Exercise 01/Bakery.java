75eaa959160873441eb42e499aea36768994769b
Mongobob <45574916+Mongobob@users.noreply.github.com>
2020-11-22 12:47:30 +0100

    Sandro 202011221247

---
+++ Assignment 05/Exercise 01/Bakery.java
@@ -0,0 +1,21 @@
+public abstract class Bakery {
+    /*
+    Bakeries: There are three different types of bakeries:
+    1. Normal bakeries , which sell both bread and sweets
+    2. Bakeries specialized in bread , which sell only bread
+    3. Bakeries specialized in sweets , which sell only sweets
+    Furthermore, each bakery has a unique name (e.g., "Bakery Crunchy Bread"),
+    an address (including the street name and number, postal code, and city).
+    Each bakery has a method "printName" to print the name of the bakery.
+    */
+    abstract public string printName();
+    abstract public string getAdress();
+
+
+}
+
+
+    
+
+
+
