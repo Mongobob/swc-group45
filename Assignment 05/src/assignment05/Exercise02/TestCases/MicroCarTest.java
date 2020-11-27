9db90403acfca559cd314dc0563f189afc30c585
JosipHarambasic <harambasic.josip97@gmail..com>
2020-11-27 11:56:22 +0100

    deleted some tests

--- Assignment 05/src/assignment05/Exercise02/TestCases/MicroCarTest.java
+++ Assignment 05/src/assignment05/Exercise02/TestCases/MicroCarTest.java
@@ -1,40 +1,57 @@
 package assignment05.Exercise02.TestCases;
 
+import assignment05.Exercise02.CarType;
+import assignment05.Exercise02.Luggage;
+import assignment05.Exercise02.VehicleCost;
+import assignment05.Exercise02.VehicleSpeed;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MicroCarTest {
 
+    CarType.MicroCar a = new CarType.MicroCar();
+    Luggage.oneBag c = new Luggage.oneBag();
+    VehicleCost.twelvePerHour f = new VehicleCost.twelvePerHour();
+    VehicleSpeed.normalSpeed g = new VehicleSpeed.normalSpeed();
+
     @Test
     void carType() {
+        assertEquals("Micro Car; ", a.carType());
     }
 
     @Test
     void getCarType() {
+        assertEquals("Micro Car; ", a.carType());
     }
 
     @Test
     void luggage() {
+        assertEquals("1 bag; ", c.amountOfLuggage());
     }
 
     @Test
     void getLuggageType() {
+        assertEquals("1 bag; ", c.amountOfLuggage());
     }
 
     @Test
     void speed() {
+        assertEquals("normal speed; ", g.speed());
     }
 
     @Test
     void getSpeed() {
+        assertEquals("normal speed; ", g.speed());
     }
 
     @Test
     void cost() {
+        assertEquals("12CHF/h", f.costPerHour());
     }
 
     @Test
     void getCost() {
+        assertEquals("12CHF/h", f.costPerHour());
     }
 }
--- Assignment 05/src/assignment05/Exercise02/TestCases/SuperCarTest.java
+++ Assignment 05/src/assignment05/Exercise02/TestCases/SuperCarTest.java
@@ -1,40 +1,58 @@
 package assignment05.Exercise02.TestCases;
 
+import assignment05.Exercise02.CarType;
+import assignment05.Exercise02.Luggage;
+import assignment05.Exercise02.VehicleCost;
+import assignment05.Exercise02.VehicleSpeed;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SuperCarTest {
 
+    CarType.SuperCar a = new CarType.SuperCar();
+    Luggage.OneSmallOneLarge c = new Luggage.OneSmallOneLarge();
+    VehicleCost.thirtyPerHour f = new VehicleCost.thirtyPerHour();
+    VehicleSpeed.fastSpeed g = new VehicleSpeed.fastSpeed();
+
+
     @Test
     void carType() {
+        assertEquals("Super Car; ", a.carType());
     }
 
     @Test
     void getCarType() {
+        assertEquals("Super Car; ", a.carType());
     }
 
     @Test
     void luggage() {
+        assertEquals("1 small and 1 large bag; ",c.amountOfLuggage());
     }
 
     @Test
     void getLuggageType() {
+        assertEquals("1 small and 1 large bag; ",c.amountOfLuggage());
     }
 
     @Test
     void speed() {
+        assertEquals("fast speed; ",g.speed());
     }
 
     @Test
     void getSpeed() {
+        assertEquals("fast speed; ",g.speed());
     }
 
     @Test
     void cost() {
+        assertEquals("30CHF/h",f.costPerHour());
     }
 
     @Test
     void getCost() {
+        assertEquals("30CHF/h",f.costPerHour());
     }
 }
