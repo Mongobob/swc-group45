cf0757039c001b135041e59ba6f68caf1e192ab8
Mongobob <45574916+Mongobob@users.noreply.github.com>
2020-12-20 16:59:52 +0100

    Delete Main.java

--- Assignment 06/src/assignment06/Exercise02/Main.java
+++
@@ -1,9 +0,0 @@
-package assignment06.Exercise02;
-
-public class Main {
-    public static void main(String[] args){
-
-        Shirt shirt = new Shirt("krass",2.99,43,"black");
-        System.out.print(shirt.getName());
-    }
-}
