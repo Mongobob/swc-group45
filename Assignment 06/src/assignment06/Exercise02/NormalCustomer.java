7567175354343f00e37189ebc75b7dc4dfcb8f66
Mongobob <45574916+Mongobob@users.noreply.github.com>
2020-12-23 12:06:33 +0100

    Update UML

--- Assignment 06/src/assignment06/Exercise02/NormalCustomer.java
+++ Assignment 06/src/assignment06/Exercise02/NormalCustomer.java
@@ -4,12 +4,11 @@ import java.util.ArrayList;
 
 
 public class NormalCustomer extends Customer {
-
+    private int itemcap =1;
 
     private NormalCustomer(String initcustomername, String initpassword){
         password = initpassword;
         customername = initcustomername;
-        itemcap=1;
     }
 
 
